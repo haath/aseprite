8b45b257a500e78cc0b07d435f8fa3bf0211d59e
Haath <haath@protonmail.com>
2022-11-06 09:37:18 +0100

    Enable ui for builds

--- push_gitea_release.py
+++ push_gitea_release.py
@@ -8,10 +8,29 @@ file = sys.argv[1]
 token = sys.argv[2]
 tag = sys.argv[3]
 
+def create_release_with_tag(tag):
+    url = f'{BASE_URL}/repos/haath/aseprite/releases/?token={token}'
+    resp = requests.post(url, data={
+        'name': tag,
+        'title': tag,
+        'tag_name': tag
+    })
+    data = resp.json()
+
+    if 'id' not in data:
+        raise Exception(f'unable to create release, error: {resp["message"]}')
+
+    return data['id']
+
 def get_release_with_tag(tag):
     url = f'{BASE_URL}/repos/haath/aseprite/releases/tags/{tag}?token={token}'
     resp = requests.get(url)
     data = resp.json()
+
+    if 'id' not in data:
+        # release doesn't exist yet, create it
+        return create_release_with_tag(tag)
+
     return data['id']
 
 def release_add_file(release_id, file_path):
